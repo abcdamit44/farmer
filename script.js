4738287b7f3ebcd6ddd199fb4205f1bb73d38cda
Amit Singh <abcdamit44@gmail.com>
2022-04-06 09:04:39 +0530

    Add Soil API

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ let form = document.getElementById('kisanFrom');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     let place = document.getElementById('place');
-    // alert(place.value)
 
     let city = place.value;
     const weather = async () => {
@@ -13,13 +12,15 @@ form.addEventListener('submit', (e) => {
     }
     const test = weather();
     let main = document.querySelector('main');
-
+    let lon;
+    let lat;
     test.then((data) => {
-        // console.log(data);
+        lon = data.coord.lon;
+        lat = data.coord.lat;
         let result = document.createElement("div");
-        let span1 = document.createElement("span");
-        let span2 = document.createElement("span");
-        let span3 = document.createElement("span");
+
+        let para = document.createElement("p");
+        para.style.fontWeight = '500';
 
         result.style.margin = "1.25vw auto";
         result.style.padding = "0.75vw";
@@ -27,16 +28,26 @@ form.addEventListener('submit', (e) => {
         result.style.backgroundColor = "#fff";
         result.style.borderRadius = "3px";
 
-        span1.append(data.name)
-        span2.append(data.main.temp)
-        span3.append(data.weather[0].description)
-        span1.style.margin = '0.75vw';
-        span2.style.margin = '0.75vw';
-        span3.style.margin = '0.75vw';
-        result.append(span1);
-        result.append(span2);
-        result.append(span3);
-        main.append(result)
+        const soil = async () => {
+            const soilData = await fetch(`https://api.ambeedata.com/soil/latest/by-lat-lng?lat=${lat}&lng=${lon}`, {
+                "method": "GET",
+                "headers": {
+                    "x-api-key": "df2017ece4ed546bb057bcc4c3eb2fc87c6515aa5f7f0c2485c595758759a59b",
+                    "Content-type": "application/json"
+                }
+            })
+            const res = await soilData.json();
+            return res;
+        }
+
+        const soilData = soil();
+        soilData.then((res) => {
+            para.innerHTML = `<b>${data.name}</b>'s current temperature is <b>${data.main.temp}°C</b>. Weather is <b>${data.weather[0].description}</b>. Soil moisture is <b>${parseInt(res.data[0].soil_moisture).toFixed(2)}°C</b> and Soil temperature is <b>${parseInt(res.data[0].soil_temperature).toFixed(2)}°C</b>`;
+            result.append(para);
+            main.append(result)
+        })
     })
 
+    form.reset()
 })
+
